b746894d4596e61aafa436ea4708e563fd7e97b1
czf <502629556@qq.com>
2018-03-30 10:56:32 +0800

    cleanup pthread

--- pthreadDemo/main.cpp
+++ pthreadDemo/main.cpp
@@ -2,36 +2,39 @@
 #include<unistd.h>
 #include<pthread.h>
 
+#define STR_SIZE 32
+
+inline void setName(char *str, int baseSize, int i) {
+	str[STR_SIZE - 1] = '\0';
+	str[baseSize] = '0' + i;
+	str[baseSize+1] = '\0';
+}
+
 void * run(void *arg) {
-    char *tname;
-    tname = (char *)arg;
-    //std::cout << "tname:" << tname << ".\n";
-    long tid_real;
-    tid_real = pthread_self();
-    std::cout << "tid:" << tid_real << ".\n";
-    return NULL;
+	char *tname;
+	tname = (char *)arg;
+	std::cout << "tname:" << tname << ".\n";
+	pthread_t tid;
+	tid = pthread_self();
+	//std::cout << "pthread_self  :" << tid << ".\n";
+	return NULL;
 }
 
 int main() {
-    int i = 7;
-    auto fn = [=]() mutable ->void {i = 777;std::cout << "i in: " << i << ".\n";};
-    fn();
-    std::cout << "i out: " << i << ".\n";
-    signed char sch; //-128~127
-    unsigned char uch; //0～255
-    char ch = 48; //unkown, one or another
-    std::cout << "ch: " << ch << ".\n";
-    std::cout << "char size:" << sizeof(char) << ".\n";
-    pthread_t pthreads[3];
-    for (int i = 0; i < 3; i++) {
-        char str[] = "new thread";
-        pthread_create(&pthreads[i], NULL, run, (void *)str);
-        //pthread_join(pthreads[i], NULL);
+	pthread_t pthreads[3];
+	for (int i = 0; i < 3; i++) {
+		int baseSize = 11;
+		char str[STR_SIZE] = "new_thread_";
+		setName(str, baseSize, i);
+		// run instantly after created.
+		pthread_create(&pthreads[i], NULL, run, (void *)str);
 
-        /* pthreads[i] == pthread_self() */
-        std::cout << "pthread_t: " << pthreads[i] << ".\n";
-    }
-    sleep(1);
-    std::cout << "main exit\n";
+		pthread_join(pthreads[i], NULL);
+
+		/* pthreads[i] == pthread_self() */
+		//std::cout << "pthread_create:" << pthreads[i] << ".\n";
+	}
+	//sleep(1);
+	std::cout << "main exit\n";
 }
 
