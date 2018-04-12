9bd6358d3c5fd5bf7e43111cd8c7717c42f993f1
czf <502629556@qq.com>
2018-04-12 12:35:30 +0800

    update notes; stack backward

--- exceptions/main.cpp
+++ exceptions/main.cpp
@@ -2,15 +2,31 @@
 #include "X.h"
 
 #include<iostream>
+#include<exception>
 
 using namespace std;
 
+void recursive(int i) {
+    if (i < 5) {
+        // 堆栈回退会销毁指针变量e，但e所指的存储不会释放。
+        // 这种情况很难找到合适的地方进行销毁，使用'资源申请即初始化'技术会方便很多。
+        MyException *e = new MyException;
+        //MyException e; // ok, stack mem.
+        recursive(++i);
+    } else {
+        throw exception();
+    }
+}
+
 int main(int argc, char *argv[]) {
 	try {
-        X x;
+        //recursive(0);
+        //X x;
         //throw MyException();
 	} catch(MyException &e) {
-		cout << "catch e\n";
-	}
+		cout << "catch MyException\n";
+    } catch(exception &ee) {
+        cout << "catch exception\n";
+    }
 	return 0;
 }
